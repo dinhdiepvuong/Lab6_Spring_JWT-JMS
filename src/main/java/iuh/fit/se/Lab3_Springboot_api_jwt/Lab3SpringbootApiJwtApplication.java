c001b1778b47e847cfe94d98d74682c1d4ea752f
dinhdiepvuong <diepvuong.py@gmail.com>
2022-11-19 11:23:48 +0700

    spring boot jwt

---
+++ src/main/java/iuh/fit/se/Lab3_Springboot_api_jwt/Lab3SpringbootApiJwtApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.Lab3_Springboot_api_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3SpringbootApiJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3SpringbootApiJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iuh/fit/se/Lab3_Springboot_api_jwt/Lab3SpringbootApiJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.Lab3_Springboot_api_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3SpringbootApiJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
